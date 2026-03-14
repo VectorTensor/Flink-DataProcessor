1202b68d1074eab919f1bd7b6d580bb372254607
Prayash Thapa <prayashthapa15@gmail.com>
2026-03-14 20:45:39 +0545

    feat:: add username, password for flink job

--- src/main/java/com/example/DataStreamJob.java
+++ src/main/java/com/example/DataStreamJob.java
@@ -16,12 +16,36 @@ import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import config.ConfigReader;
 
+import java.io.ObjectInputFilter;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+
+
 public class DataStreamJob{
 
+    public class MongoUtils {
+
+        public static String buildMongoUrl(String username, String password, String hostname) {
+            String authPart = "";
+
+            if (username != null && !username.isEmpty() && password != null) {
+                // Encode password safely
+                String encodedPassword = URLEncoder.encode(password, StandardCharsets.UTF_8);
+                authPart = String.format("%s:%s@", username, encodedPassword);
+            }
+
+            return String.format("mongodb://%s%s", authPart, hostname);
+        }
+    }
     public static void main(String[] args) throws Exception {
         // kafka-headless.kafka:9092
         String config_kafka = ConfigReader.get("CFG_KAFKA_DOMAIN");
-        String config_mongo = ConfigReader.get("CFG_MONGO_URL");
+        String mongo_user = ConfigReader.get("CFG_MONGO_USER");
+        String mongo_password = ConfigReader.get("CFG_MONGO_PASSWORD");
+        String mongo_host = ConfigReader.get("CFG_MONGO_HOST");
+
+        String config_mongo = MongoUtils.buildMongoUrl(mongo_user, mongo_password, mongo_host);
 
         System.out.printf("KAFKA : %s , MONGO_URL : %s", config_kafka, config_mongo);
         StreamExecutionEnvironment env =
